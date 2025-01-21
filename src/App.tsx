3ce449dc5354436f1ab7d3c5ba0c8d6890e50665
victoriaw2408 <victoriaw2408@gmail.com>
2025-01-21 00:14:47 -0500

    worked on assignment 1 lab exercises

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-
-import Labs from './Labs';
-import { HashRouter, Route, Routes, Navigate} from "react-router";
-// import './App.css'
-
+import Labs from "./Labs";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 function App() {
-
   return (
-    <h1> Welcome to Web Dev </h1>
+    <HashRouter>
+      <div>
+        <Routes>
+        <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
---
+++ src/Labs/Lab1/index.tsx
@@ -0,0 +1,148 @@
+export default function Lab1() {
+    return (
+        <div>
+            <h2> Lab 1</h2>
+            {/* <h2>Lab 1</h2> */}
+            <h3>HTML Examples</h3>
+            <div id="wd-h-tag">
+                <h4>Heading Tags</h4>
+                Text documents are often broken up into several sections and subsections.
+                Each section is usually prefaced with a short title or heading that attempts to
+                summarize the topic of the section it precedes. For instance this paragraph is preceded
+                by the heading Heading Tags. The font of the section headings are usually larger and bolder
+                than their subsection headings. This document uses headings to introduce topics such as HTML Documents,
+                HTML Tags, Heading Tags, etc. HTML heading tags can be used to format plain text so that it renders in a
+                browser as large headings. There are 6 heading tags for different sizes: h1, h2, h3, h4, h5, and h6. Tag
+                h1 is the largest heading and h6 is the smallest heading.
+            </div>
+            <div id="wd-p-tag">
+                <h4>Paragraph Tag</h4>
+                <p id="wd-p-1">
+                    This is a paragraph. We often separate a long set of sentences with vertical spaces to make the text easier to read. Browsers ignore vertical white spaces and render all the text as one single set of sentences. To force the browser to add vertical spacing, wrap the paragraphs you want to separate with the paragraph tag
+                </p>
+                <p>
+                    This is the first paragraph. The paragraph tag is used to format vertical gaps between long pieces of text like this one.
+                </p>
+                <p>
+                    This is the second paragraph. Even though there is a deliberate white gap between the paragraph above and this paragraph, by default browsers render them as one contiguous piece of text as shown here on the right.
+                </p>
+                <p>
+                    This is the third paragraph. Wrap each paragraph with the paragraph tag to tell browsers to render the gaps.
+                </p>
+            </div>
+            <div id="wd-lists">
+                <h4>List Tags</h4>
+                <h5>Ordered List Tag</h5>
+                How to make pancakes:
+                <ol id="wd-pancakes">
+                    <li>Mix dry ingredients.</li>
+                    <li>Add wet ingredients.</li>
+                    <li>Stir to combine.</li>
+                    <li>Heat a skillet or griddle.</li>
+                    <li>Pour batter onto the skillet.</li>
+                    <li>Cook until bubbly on top.</li>
+                    <li>Flip and cook the other side.</li>
+                    <li>Serve and enjoy!</li>
+                </ol>
+                My favorite recipe:
+                How to make cereal!
+                <ol id="wd-your-favorite-recipe">
+                    <li>Get milk from fridge.</li>
+                    <li>Add milk to bowl.</li>
+                    <li>Take out cereal.</li>
+                    <li>Put cereal in bowl.</li>
+                    <li>Get spoon.</li>
+                    <li>Eat cereal!</li>
+                </ol>
+            </div>
+            <div>
+                <h5>Unordered List Tag</h5>
+                My favorite books (in no particular order)
+                <ul id="wd-my-books">
+                    <li>Dune</li>
+                    <li>Lord of the Rings</li>
+                    <li>Ender's Game</li>
+                    <li>Red Mars</li>
+                    <li>The Forever War</li>
+                </ul>
+                Your favorite books (in no particular order)
+                <ul id="wd-your-books">
+                    <li>Dune</li>
+                    <li>Poppy War</li>
+                    <li>Silent Patient</li>
+                    <li>Remarried Empress</li>
+                    <li>Teenage Mercenary</li>
+                    <li>Sweet Home</li>
+                </ul>
+            </div>
+            <div>
+                <h4>Form elements</h4>
+                <label htmlFor="username"> Username:</label>
+                <input
+                    id="username"
+                    type="text"
+                    placeholder="jdoe"
+                    title="Please type your username which should be unique."
+                />
+                <br />
+            </div>
+            <div>
+                <h4>Form elements</h4>
+                <label htmlFor="password"> Password:</label>
+                <input
+                    id="password"
+                    type="password"
+                    placeholder="jdoe"
+                    title="Please type your password which doesn't have to be unique."
+                />
+                <br />
+                <button type="button">
+                    Delete
+                </button>
+                <br />
+                <button type="button">
+                    Edit
+                </button>
+                <br />
+                <button type="submit">
+                    Update
+                </button>
+                <br />
+                <h5 id="wd-buttons">Buttons</h5>
+                <button id="wd-all-good"
+                    onClick={() => alert("Life is Good!")}
+                    type="button">
+                    Hello World!
+                </button>
+
+                <h5 id="wd-radio-buttons">Radio buttons</h5>
+
+                <label>Favorite movie genre:</label><br />
+                <input id="comedy" type="radio" name="radio-genre"/> 
+                <label htmlFor="comedy"> Comedy</label>
+                <br />
+                <input id="drama" type="radio" name="radio-genre"/> 
+                <label htmlFor="drama"> Drama</label> <br />
+                <input id="scifi" type="radio" name="radio-genre"/> 
+                <label htmlFor="scifi">Sci-Fi</label> <br />
+                <input id="horror" type="radio" name="radio-genre"/> 
+                <label htmlFor="horror">Horror</label> <br />
+
+                <label>Favorite color:</label><br />
+                <input id="red" type="radio" name="color"/> 
+                <label htmlFor="red">Red</label> <br />
+                <input id="yellow"  type="radio" name="color"/> 
+                <label htmlFor="yellow">Yellow </label><br />
+                <input id="blue" type="radio" name="color"/> 
+                <label htmlFor="blue"> Blue</label> <br />
+                <input id="green" type="radio" name="color"/> 
+                <label htmlFor="green"> Green </label><br />
+
+
+
+
+
+            </div>
+        </div>
+    );
+}
