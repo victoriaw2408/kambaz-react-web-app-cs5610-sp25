bd3b2e773ead0d0176392bb872c17196db064aa3
victoriaw2408 <victoriaw2408@gmail.com>
2025-01-22 23:29:18 -0500

    adding courses

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -17,8 +17,79 @@ export default function Dashboard() {
             </div>
           </Link>
         </div>
-        <div className="wd-dashboard-course"> ... </div>
-        <div className="wd-dashboard-course"> ... </div>
+        <div className="wd-dashboard-course">
+          <Link to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link" >
+            <img src="/images/unnamed.jpg" width={200} />
+            <div>
+              <h5> ASTRO1001  </h5>
+              <p className="wd-dashboard-course-title">
+                Astronomy  </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link" >
+            <img src="/images/Recipe_chocolate_layer_cake-1000x1000.jpg" width={200} />
+            <div>
+              <h5> BAKE2000  </h5>
+              <p className="wd-dashboard-course-title">
+                Cake Baking  </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link" >
+            <img src="/images/images.jpg" width={200} />
+            <div>
+              <h5> CY3000  </h5>
+              <p className="wd-dashboard-course-title">
+                Cybersecurity Specialist  </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link" >
+            <img src="/images/download.jpg" width={200} />
+            <div>
+              <h5> ART1003  </h5>
+              <p className="wd-dashboard-course-title">
+                Art Branding  </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link" >
+            <img src="/images/download.png" width={200} />
+            <div>
+              <h5> MATH3000  </h5>
+              <p className="wd-dashboard-course-title">
+                Linear Algebra </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link" >
+            <img src="/images/download-1.jpg" width={200} />
+            <div>
+              <h5> ARCH2025  </h5>
+              <p className="wd-dashboard-course-title">
+                Skyscraper Architecture  </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+       
       </div>
     </div>
 );}
