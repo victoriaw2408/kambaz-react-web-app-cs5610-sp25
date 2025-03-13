5871e7cd0070e6e6c49ff0afefc0ecf1ce14023d
victoriaw2408 <victoriaw2408@gmail.com>
2025-03-13 01:16:20 -0400

    fix

--- src/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
+++ src/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
@@ -1,5 +1,3 @@
-
-import React from "react";
 import { useDispatch } from "react-redux";
 import { deleteTodo, setTodo } from "./todosReducer";
 import { Button, Form, ListGroup } from "react-bootstrap";
